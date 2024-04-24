49df3ea5638532a72b3a65794399cc95376a1700
nicolas00r <163439327+nicolas00r@users.noreply.github.com>
2024-04-24 19:08:23 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,7 +45,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
     while(map->buckets[pos] != NULL && map->buckets[pos]->key != NULL){
       pos++;
-      //if(pos >= map->size) pos = 0;
+      if(pos > map->size) pos = 0;
     } 
     map->buckets[pos] = newPair;
     map->size++;
