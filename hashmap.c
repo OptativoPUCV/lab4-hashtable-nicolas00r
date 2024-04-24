6ea333bf158040329ebab89e1ea32659d293c83a
nicolas00r <163439327+nicolas00r@users.noreply.github.com>
2024-04-24 19:36:44 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -110,7 +110,7 @@ Pair * nextMap(HashMap * map) {
     long pos = map->current + 1;
     while(map->buckets[pos] == NULL || map->buckets[pos]->key == NULL){
       pos++;
-      if(pos == map->capacity) pos = 0;
+      if(pos >= map->capacity) pos = 0;
       if(pos == map->current) return NULL;
     }
     map->current = pos;
